2813e7133b0f8f29090f8d3edcde8f70ca7424f9
Justin Patriquin <justinpatriquin@gmail.com>
2012-11-20 15:05:46 -0400

    Added a template for the class definitions that will be in the Workflow Engine

---
+++ WorkflowEngine.h
@@ -0,0 +1,58 @@
+/*
+ *	Name: 			  Justin Patriquin
+ *	Date: 			  November 15, 2012
+ * 	Organization:	Dalhousie - Faculty of Computer Science
+ *	
+ * 	Description:
+ */
+#pragma once
+
+class Edge{
+  public:
+    int getType(void);
+    void setType(int);
+  private:
+    int type;
+};
+
+class AbstractNode{
+  public:
+
+  private:
+
+};
+
+class Task : AbstractNode{
+  public:
+
+  private:
+
+};
+
+class WorkflowGraph{
+  public:
+
+  private:
+  
+};
+
+class AbstractNodeFactory{
+  public:
+
+  private:
+
+};
+
+class TaskFactory : AbstractNodeFactory{
+  public:
+
+  private:
+
+};
+
+class WorkflowLoader{
+  public:
+
+  private:
+
+};
